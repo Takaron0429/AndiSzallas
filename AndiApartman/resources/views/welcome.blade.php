6e510e8727235b796cfad16309ce0f60660dfe61
Takaron0429 <takaron0429@gmail.com>
2025-01-20 19:52:33 +0100

    alapok

---
+++ AndiApartman/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Andi Apartman</title>
+</head>
+<body>
+    <h1>Ez mi?</h1>
+</body>
+</html>
